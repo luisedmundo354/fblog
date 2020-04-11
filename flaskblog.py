70fc63580cd92060408282cf0274641532ba80b6
luisedmundo354 <luisedmundo354@gmail.com>
2020-04-10 20:55:06 -0400

    nothing to commit

--- flaskblog.py
+++ flaskblog.py
@@ -8,14 +8,20 @@ posts = [
   {
     'author': 'Daniela Flores',
     'title': 'The book about you',
-    'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget velit aliquet libero venenatis ultrices at sit amet felis. Mauris at justo risus. Fusce efficitur imperdiet nunc, at aliquam nisl porttitor vel. Etiam molestie arcu elit, et pulvinar justo varius efficitur. Donec laoreet vehicula augue, eu auctor lectus placerat quis. Cras congue, tellus sit amet malesuada dictum, ligula metus convallis magna, ac fermentum neque elit in massa. Aliquam nec mauris risus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Fusce ipsum nibh, posuere interdum ullamcorper et, iaculis ac nulla. Aliquam erat volutpat. Donec eu dolor.',
+    'content': "No there is no other way, I believe in many ways, I'm trapped in this place. I'm trying to find the light in here, wanting the reasons to be enough, falling in you. Sometimes I cannot forget the path . I want to suffer anymore, no I don't.",
     'date_posted': 'November 22, 2019'
   },
   {
     'author': 'Jose Brena',
     'title': 'About cars',
-    'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget velit aliquet libero venenatis ultrices at sit amet felis. Mauris at justo risus. Fusce efficitur imperdiet nunc, at aliquam nisl porttitor vel. Etiam molestie arcu elit, et pulvinar justo varius efficitur. Donec laoreet vehicula augue, eu auctor lectus placerat quis. Cras congue, tellus sit amet malesuada dictum, ligula metus convallis magna, ac fermentum neque elit in massa. Aliquam nec mauris risus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Fusce ipsum nibh, posuere interdum ullamcorper et, iaculis ac nulla. Aliquam erat volutpat. Donec eu dolor.',
+    'content': "No hay otro camino, confio en los caminos, aunque no sean correctos. Es tan facil no estar solo, pero tan dificil sentirse acompañado. Me da miedo ser juzgado, a enfrentar el fondo de esto. Aunque no te tenga, me tendré a mi. Aunque no sueñe, tu eres lo único que imagino. Y si no te tengo, por lo menos me tendré a mi. No culpes mis estados, mis pecados, mis Amores, mis sentidos y lo que no puedo controlar.",
     'date_posted': 'November 23, 2019'
+  },
+  {
+    'author': 'Luis Breña',
+    'title': 'About you',
+    'content': "Nunca pienso dos veces, porque tu piensas lo mismo. Todos saben que no siento, pero si no te tengo, por lo menos me tengo a mi. Ya no soy dueño de mi voluntad, de mis pensamientos. Tengo que abrazar el sentimiento, la fuerza del espíritu, mi resplandor. ",
+    'date_posted': 'March 16, 2020'
   }
 ]
 
